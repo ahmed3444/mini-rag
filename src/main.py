8087a5ca1dd13c64675c124cc40105d936a30139
mosalam <ahmedmosalam46@gmail.com>
2025-08-01 19:56:15 +0300

    import to base and data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
+from src.routes import base
+from src.routes import data
 
-load_dotenv(".env")
-from routes import base
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)

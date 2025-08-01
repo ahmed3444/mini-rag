2279ba8e53806a9db902192f40683c9c8bfa11ea
mosalam <ahmedmosalam46@gmail.com>
2025-08-01 19:54:24 +0300

    creat class config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-class Config(BaseSettings):
+class Settings(BaseSettings):
     App_NAME: str 
     App_DESCRIPTION: str
     App_VERSION: str   
-    
+
     class Config:
         env_file = ".env"
-        
+
+def get_settings() :
+    return Settings()
+
